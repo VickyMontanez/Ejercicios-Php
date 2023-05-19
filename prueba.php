52a1280e29c020c79d4e46c3f2db4157337698bf
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-05-19 01:29:10 -0500

    feat: :rocket: Deploy a new exercises of Php
    Deploy a basic exercise to learn Php

---
+++ prueba.php
@@ -0,0 +1,14 @@
+<!-- NOTAS DE CURSO PHP -->
+<!-- 1. Tener instalada la extensión de vscode "Php Extension Pack" para que visual asimile el lenguaje de php -->
+<!-- PHP como HTML tiene un principio y un final por eso se utiliza etiqueta para abrir y cerrar -->
+<?php
+/* se puede imprimir en el documento con print o echo */
+echo "Hola Mundo";
+/* PHP permite leer codigo de HTML */
+echo "<h2>Esta es una etiqueta HTML en PHP</h2>";
+
+echo "<h2>Esta es una etiqueta HTML en PHP pero no acepta ESTILOS</h2>";
+
+print "Hola Mundox2";
+
+?>
